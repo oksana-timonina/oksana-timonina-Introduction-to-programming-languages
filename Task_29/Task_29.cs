87f30494810541a215bba9e8185a209877d7640a
Oksana Timonina <oks.timonina95@yandex.ru>
2022-12-13 19:02:43 +0300

    Write program Task_29

---
+++ Task_29/Task_29.cs
@@ -0,0 +1,14 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int [] array = GetArray(8);
+Console.WriteLine ($"Массив: [{String.Join (",", array)}]");
+
+int [] GetArray (int size)
+{
+    int [] result = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        result [i] = new Random().Next(10);
+    }
+    return result;
+}
