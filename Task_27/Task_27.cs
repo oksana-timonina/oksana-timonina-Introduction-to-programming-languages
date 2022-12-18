0979e7d35c12dc2ae5d51aa5d9fea412384c3bf8
Oksana Timonina <oks.timonina95@yandex.ru>
2022-12-18 12:54:02 +0300

    Upgrade program Task_27

--- Task_27/Task_27.cs
+++ Task_27/Task_27.cs
@@ -6,6 +6,9 @@
 
 9012 -> 12 */
 
+// 1 вариант программы
+/*
+
 Console.Write ("Введите число: ");
 int num = int.Parse (Console.ReadLine()!); 
 if (num < 0) 
@@ -27,4 +30,38 @@ int GetSumNums (int number)
         number/=10;
     }
     return sum;
+}
+*/
+// 2 вариант программы
+
+Console.Write ("Введите число: ");
+int num = int.Parse (Console.ReadLine()!); 
+Console.WriteLine ($"Сумма цифр в числе {num} равна {GetSumNums(num)}"); 
+
+//-------------МЕТОД---------------
+int GetSumNums (int number) 
+{
+    int sum = 0;
+    bool flag = false;
+    if (number < 0) 
+{
+    flag = true;
+    number = number*-1; 
+}
+
+    while (number>10)
+    {
+        sum = sum + number%10;
+        number/=10;
+    }
+
+    if(flag)
+    {
+        return sum - number;
+    }
+    else
+    {
+        return sum + number;
+    }
+    
 }
